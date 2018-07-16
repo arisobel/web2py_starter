35688da3c79dfa0e975da6de47d387ee802e7850
Miguel Antonio Conde <macg9106@gmail.com>
2018-07-16 14:36:15 -0400

    Spanish language added
    Added support to force translation to spanish language in the application.

--- models/0.py
+++ models/0.py
@@ -12,17 +12,20 @@ import sys
 os_plataform = platform.system()
 configs.langs = {
                     'br' : {'lang':'pt-br', 'initial':'PT','name':T('Portuguese')},
+                    'es' : {'lang':'es', 'initial':'ES','name':T('Español')},
                     'us' : {'lang':'default', 'initial':'EN','name':T('English')},
                     }
 if os_plataform == "Windows":
     configs.langs['il'] = {'lang':'he', 'initial':'HE', 'name':T('Hebrew')}
     configs.langs['il']['locale'] = 'hebrew_israel.1252'
     configs.langs['br']['locale'] = 'portuguese_brazil.1252'
+    configs.langs['es']['locale'] = 'español_España.1252'
     configs.langs['us']['locale'] = 'English_United States.1252'
                     
 elif os_plataform == "Linux":
     #configs.langs['il']['locale'] = 'he_IL.utf8'
     configs.langs['br']['locale'] = 'pt_BR.UTF-8'
+    configs.langs['es']['locale'] = 'es_CU.UTF-8'
     configs.langs['us']['locale'] = 'en_US.UTF-8'
 
 
